fe8d95b5ae67b5ebcb86662e74f746a6eaf2f915
guts <the_guts@outlook.com>
2024-05-22 17:24:20 +0330

    bootstrap setup also home edited

--- ChatServerPlayground/urls.py
+++ ChatServerPlayground/urls.py
@@ -14,12 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
-
 from home.views import home_screen_view
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_screen_view),
 ]
+
+
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
